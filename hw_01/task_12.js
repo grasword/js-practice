3a9c51a124ea40b51ef62df0f7f7cf0ef58b3279
Oleg Zhmaev <grasword6@gmail.com>
2022-02-21 14:40:38 +0200

    solved twelfth task [task 12]-[hw1-js]

---
+++ hw_01/task_12.js
@@ -0,0 +1,54 @@
+// 1. Create a personAccount out function. It has firstname, lastname, incomes, expenses  inner variables.
+//    It has totalIncome, totalExpense, accountInfo, addIncome, addExpense and accountBalance inner functions.
+//    Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+
+const personAccount = function () {
+  let firstName = 'John'
+  let lastName = 'Doe'
+  let incomes = [
+    { income: 2000, description: 'salary' },
+    { income: 400, description: 'courses' }
+  ]
+  let expenses = [
+    { expense: 150, description: 'taxi' },
+    { expense: 400, description: 'bar' }
+  ]
+  const totalIncome = function () {
+    return incomes.reduce((a, b) => a + b.income, 0)
+  }
+  const totalExpense = function () {
+    return expenses.reduce((a, b) => a + b.expense, 0)
+  }
+  const accountInfo = function () {
+    return `${firstName} ${lastName}\n
+    Total incomes = ${totalIncome()}\n
+    Total expenses = ${totalExpense()}\n
+    Account balance = ${accountBalance()}`
+  }
+  const addIncome = function (income, description) {
+    incomes.push({ income, description })
+  }
+  const addExpense = function (expense, description) {
+    expenses.push({ expense, description })
+  }
+  const accountBalance = function () {
+    return totalIncome() - totalExpense()
+  }
+  return {
+    totalIncome,
+    totalExpense,
+    accountInfo,
+    addIncome,
+    addExpense,
+    accountBalance
+  }
+}
+
+const person = personAccount()
+
+console.log(person.totalIncome())
+console.log(person.totalExpense())
+person.addIncome(450, 'gift')
+person.addExpense(700, 'massage')
+console.log(person.accountBalance())
+console.log(person.accountInfo())
