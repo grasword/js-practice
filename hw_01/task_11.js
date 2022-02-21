2941fc5112b8989d1e93cb8843696e34370bc06c
Oleg Zhmaev <grasword6@gmail.com>
2022-02-21 18:13:55 +0200

    solved eleventh task  [task 11]-[hw1-js]

---
+++ hw_01/task_11.js
@@ -0,0 +1,127 @@
+const student = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayehe',
+  age: 250,
+  isMarried: true,
+  skills: ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python']
+}
+
+const txt = `{
+    "Alex": {
+        "email": "alex@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 30
+    },
+    "Asab": {
+        "email": "asab@asab.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Redux",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 25,
+        "isLoggedIn": false,
+        "points": 50
+    },
+    "Brook": {
+        "email": "daniel@daniel.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux"
+        ],
+        "age": 30,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Daniel": {
+        "email": "daniel@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "John": {
+        "email": "john@john.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux",
+            "Node.js"
+        ],
+        "age": 20,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Thomas": {
+        "email": "thomas@thomas.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "Paul": {
+        "email": "paul@paul.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    }
+}
+`
+
+// 1. Stringify the students object with only firstName, lastName and skills properties
+const getStudent = (s) => JSON.stringify(s, ['firstName', 'lastName', 'skills'], 2)
+
+console.log(getStudent(student))
+
+// 2. Parse the txt JSON to object
+const parseToObj = (json) => JSON.parse(json)
+
+console.log(parseToObj(txt))
+
+// 3. Find the user who has many skills from the variable stored in txt
+const mostSkilled = (json) => {
+  const users = parseToObj(json)
+  const usersTuple = Object.entries(users)
+
+  const mostSkilled = usersTuple.reduce((p, c) => {
+    return p[1].skills.length > c[1].skills.length ? p : c
+  })
+  return `${mostSkilled[0]} has skills ${mostSkilled[1].skills.join(', ')}`
+}
+
+console.log(mostSkilled(txt))
