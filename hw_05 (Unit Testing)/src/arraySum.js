c83fea152369a772e87c2daf7d2aec8150148362
Oleg Zhmaev <grasword6@gmail.com>
2022-03-10 21:49:33 +0200

    solved the `arraySum` task [UT]-[hw5-js]

---
+++ hw_05 (Unit Testing)/src/arraySum.js
@@ -0,0 +1,23 @@
+/**
+ * It recieves an array of strings, integers and array like itself.
+ * Return the summary of all integers in it on * any level.
+ *
+ * @param {Array} elements
+ * @returns {number} summary of all integers or 0 in other cases
+ */
+function arraySum(elements) {
+  // PLACE YOUR CODE BETWEEN THIS...
+  const toNumber = (num) => {
+    return typeof num === 'number' ? num : 0;
+  };
+  const flatten = elements.flat(Infinity);
+  if (flatten.length <= 1) {
+    return 0;
+  }
+  return flatten.reduce((sum, num) => {
+    return sum + toNumber(num);
+  }, 0);
+  // ...AND THIS COMMENT LINE!
+}
+
+module.exports = arraySum;
