dc7d92035a8888f5c7b7229ad1e8af3caa02a77a
Oleg Zhmaev <grasword6@gmail.com>
2022-03-11 18:13:22 +0200

    solved the `properties` task [UT]-[hw5-js]

--- hw_05 (Unit Testing)/src/properties.js
+++ hw_05 (Unit Testing)/src/properties.js
@@ -4,3 +4,17 @@
  * @param {object} obj the object
  * @returns {string[]} the list of the properties of the object or empty array if it is not an object
  */
+const properties = (obj) => {
+  if (typeof obj !== 'object') {
+    return [];
+  }
+  const arrOfProp = [];
+  for (const prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      arrOfProp.push(prop);
+    }
+  }
+  return arrOfProp;
+};
+
+module.exports = properties;
