d780119e25f281087f8511cddd7fac251a18da7d
Oleg Zhmaev <grasword6@gmail.com>
2022-03-12 18:04:21 +0200

    solved the `toCamelCase` task [UT]-[hw5-js]

--- hw_05 (Unit Testing)/src/toCamelCase.js
+++ hw_05 (Unit Testing)/src/toCamelCase.js
@@ -7,7 +7,18 @@
  */
 function toCamelCase(toConvert) {
   // PLACE YOUR CODE BETWEEN THIS...
-
+  if (typeof toConvert !== 'string') {
+    return '';
+  }
+  const regExp = /\w+|\n\w+|\t\w+/g;
+  const words = toConvert.match(regExp);
+  const lowerCaseWords = words.map((w) => {
+    return w.toLowerCase();
+  });
+  const camelCaseWords = lowerCaseWords.map((w, i) => {
+    return i === 0 ? w : w.charAt(0).toUpperCase() + w.slice(1);
+  });
+  return camelCaseWords.join('').replace(/[\s]+/g, '');
   // ...AND THIS COMMENT LINE!
 }
 
