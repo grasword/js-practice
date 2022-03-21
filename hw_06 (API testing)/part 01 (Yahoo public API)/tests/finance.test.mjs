4c2fcf911c6d9256b90cf2aaf862a2a2306d08d2
Oleg Zhmaev <grasword6@gmail.com>
2022-03-21 13:49:15 +0200

    solved task №3 [API]-[hw6-js]

--- hw_06 (API testing)/part 01 (Yahoo public API)/tests/finance.test.mjs
+++ hw_06 (API testing)/part 01 (Yahoo public API)/tests/finance.test.mjs
@@ -56,13 +56,45 @@ describe('As a FinanceModel API user', () => {
     })
 
     test('x-request-id', async () => {
-      const { headers } = await FinanceModel.getFinanceData({ company: 'TSLA', interval: '1m', range: '1m' })
+      const { headers } = await FinanceModel.getFinanceData({ company: 'TSLA', interval: '1m', range: '1d' })
 
       //  TODO: insert verifications for "x-request-id" header
       //  You have to use regular expression to verify a headers unique value:
       //  "d5f838b8-fe84-4f2a-b950-7a07f13264a7"
       const regex = /^[a-zA-Z\d]{8}-[a-zA-Z\d]{4}-[a-zA-Z\d]{4}-[a-zA-Z\d]{4}-[a-zA-Z\d]{12}/g
       expect(headers['x-request-id']).toMatch(regex)
+    })
+  })
+
+  describe('As a FinanceModel API user', () => {
+    test('I have to get HTTP response body with [symbol] property', async () => {
+      const arg = 'TSLA'
+      const response = await FinanceModel.getFinanceData({ company: arg })
+      expect(response.data.chart.result.some((entry) => {return entry.meta.symbol === arg})).toBeTruthy()
+    })
+    
+    test('I have to get HTTP response body with [range] property', async () => {
+      const arg = '1m'
+      const response = await FinanceModel.getFinanceData({ company: 'TSLA', range: arg })
+      expect(response.data.chart.result.some((entry) => {return entry.meta.range === arg})).toBeTruthy()
+    })
+
+    test('I have to get HTTP response body with [tradingPeriods] property', async () => {
+      const response = await FinanceModel.getFinanceData({ company: 'TSLA' })
+      expect(response.data.chart.result.some((entry) => {return entry.meta.tradingPeriods.length})).toBeTruthy()
+    })
+
+    test('I have to get HTTP response body with [currentTradingPeriod] property', async () => {
+      const response = await FinanceModel.getFinanceData({ company: 'TSLA' })
+      const currentTradingPeriod = response.data.chart.result.map((entry) => {return entry.meta.currentTradingPeriod})
+      expect(currentTradingPeriod[0]).toHaveProperty('pre')
+      expect(currentTradingPeriod[0]).toHaveProperty('regular')
+      expect(currentTradingPeriod[0]).toHaveProperty('post')
+    })
+
+    test('I have to get HTTP response body with [error] property', async () => {
+      const response = await FinanceModel.getFinanceData({ company: 'TSLA', range: '10y' })
+      expect(response.data.chart.error.code).toBe('Unprocessable Entity')
     })
   })
 })
