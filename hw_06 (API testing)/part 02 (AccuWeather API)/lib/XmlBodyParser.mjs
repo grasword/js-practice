7c4669ad92cec46475b6a2b59467b2d9e20d9a34
Oleg Zhmaev <grasword6@gmail.com>
2022-03-22 21:25:58 +0200

    implemented working solution for `getItems` method in task №4 [API]-[hw6-js]

--- hw_06 (API testing)/part 02 (AccuWeather API)/lib/XmlBodyParser.mjs
+++ hw_06 (API testing)/part 02 (AccuWeather API)/lib/XmlBodyParser.mjs
@@ -17,6 +17,33 @@ class XmlBodyParser {
 
   static async getItems(httpResponseBody) {
     //TODO: extract and return Item node list
+    const documentObject = new DOMParser().parseFromString(httpResponseBody, 'text/xml')
+    const channelElement = documentObject.getElementsByTagName('rss')[0].getElementsByTagName('channel')[0]
+
+    const contentPropertiesList = channelElement.childNodes
+    const items = []
+
+    for (let i = 0; i < contentPropertiesList.length; i++) {
+      const property = contentPropertiesList.item(i)
+      if (property.nodeName === 'item') {
+        const itemNodes = {}
+        const itemChildNodes = property.childNodes
+
+        for (let k = 0; k < itemChildNodes.length; k++) {
+          const prop = itemChildNodes.item(k)
+          if (prop.nodeName === 'guid') {
+            const permalinkValue = prop.getAttribute('isPermaLink')
+            itemNodes[prop.nodeName] = { text: prop.textContent, isPermaLink: permalinkValue }
+          } else {
+            itemNodes[prop.nodeName] = prop.textContent
+          }
+        }
+        items.push(itemNodes)
+      }
+    }
+
+    // console.log(items)
+    return items
   }
 }
 
