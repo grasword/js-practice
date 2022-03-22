23d10bdcba38828b98f2e7d423e29d9c89a29ae8
Oleg Zhmaev <grasword6@gmail.com>
2022-03-22 21:23:37 +0200

    refacored `getFinanceData` method for task №1 [API]-[hw6-js]

--- hw_06 (API testing)/part 01 (Yahoo public API)/model/FinanceModel.mjs
+++ hw_06 (API testing)/part 01 (Yahoo public API)/model/FinanceModel.mjs
@@ -1,5 +1,4 @@
-/* eslint-disable semi */
-/* eslint-disable indent */
+/* eslint-disable */
 import AppConfig from '../configs/AppConfig.cjs'
 import axios from 'axios'
 
@@ -7,14 +6,16 @@ class FinanceModel {
   // TODO: define parameters to be able to get dynamic test data
   static async getFinanceData(paramsPassedToFunction) {
     const { company, ...queryParams } = paramsPassedToFunction
-    const baseUrl = AppConfig.baseUrl + company
+    const url = new URL(company, AppConfig.baseUrl)
     //  we dont need defaults actually
     //  const defaults = { lang: 'en-US', includePrePost: 'false', corsDomain: 'finance.yahoo.com', tsrc: 'finance' }
-    const params = new URLSearchParams(queryParams)
-    const href = new URL(`${baseUrl}?${params}`).href
+    // append all search params to url without worrying about `?` symbol
+    for (const [key, value] of Object.entries(queryParams)) {
+      url.searchParams.append(key, value.toString())
+    }
 
     try {
-      return await axios.get(href)
+      return await axios.get(url.href)
     } catch (e) {
       return e.response
     }
