8c1208dfcfbfe6f1521e29a80033899c0843503f
Oleg Zhmaev <grasword6@gmail.com>
2022-03-22 21:27:32 +0200

    added second test for task №4 [API]-[hw6-js]

--- hw_06 (API testing)/part 02 (AccuWeather API)/tests/weather.test.mjs
+++ hw_06 (API testing)/part 02 (AccuWeather API)/tests/weather.test.mjs
@@ -14,8 +14,7 @@ describe('As a Weather API user', () => {
 
     test('with [Channel] items', async () => {
       const { data } = await WeatherModel.getWeatherData()
-      const items = XmlBodyParser.getItems(data)
-
+      const items = await XmlBodyParser.getItems(data)
       //  TODO: insert verifications to check that every item node contains the following child nodes:
       // - title
       // - link
@@ -26,6 +25,18 @@ describe('As a Weather API user', () => {
       //  TODO: the verifications should check that:
       // - link node value contains a href
       // - guid node has "isPermaLink" attribute
+
+      items.forEach((item) => {
+        expect(item).toHaveProperty('title')
+        expect(item).toHaveProperty('link')
+        expect(item).toHaveProperty('guid')
+        expect(item).toHaveProperty('description')
+        expect(item).toHaveProperty('pubDate')
+
+        const urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:\/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm
+        expect(item.link).toMatch(urlRegex)
+        expect(item.guid).toHaveProperty('isPermaLink')
+      })
     })
   })
 })
