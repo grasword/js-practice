964682bdd1ee410d11391bab4b3c23308394d52f
Oleg Zhmaev <grasword6@gmail.com>
2022-03-26 18:08:02 +0200

    implemented solution for second test case `expiration time` [API]-[hw6-js]

--- hw_06 (API testing)/part 03 (Pastebin API)/tests/paste.test.mjs
+++ hw_06 (API testing)/part 03 (Pastebin API)/tests/paste.test.mjs
@@ -1,5 +1,7 @@
+/* eslint-disable */
 import PasteModel from '../model/PasteModel'
 import InputTestDataProvider from '../lib/InputTestDataProvider'
+import XmlBodyParser from '../lib/XmlBodyParser.mjs'
 
 describe('As a Paste API user', () => {
   describe('I need to be able to create Paste and get', () => {
@@ -17,13 +19,20 @@ describe('As a Paste API user', () => {
       // TODO: Develop "getTestData" method to extract file content from files located on "<projectDir>/data/post_data" directory
       // to be able to define POST body dynamically
       const testData = InputTestDataProvider.getTestData('case1.json')
-      // console.log(testData)
-      const { data } = await PasteModel.postPaste(testData, 'pastbin_token_1')
-      console.log(data)
+      const { data: url } = await PasteModel.postPaste(testData, 'pastbin_token_1')
+      const key = PasteModel.getKeyFromUrl(url)
 
+      const listData = InputTestDataProvider.getTestData('case2.json')
+      const { data } = await PasteModel.postPaste(listData, 'pastbin_token_1')
+
+      const expirationDate = await XmlBodyParser.getPropValue(data, key, 'paste_expire_date')
+      const creationDate = await XmlBodyParser.getPropValue(data, key, 'paste_date')
+
+      const expirationTime = parseInt(expirationDate) - parseInt(creationDate)
+      const TEN_MINUTES = 600
       // put your verifications here. You need to verify that the created Paste has been created with 10 min
       // expiration timeout
-      // ...
+      expect(expirationTime).toBe(TEN_MINUTES)
     })
   })
 })
