d3449209c9322aaa598970ae1fa170e2c339ff8d
Oleg Zhmaev <grasword6@gmail.com>
2022-04-09 16:40:46 +0300

    implemented test steps for `Screener` page [BDD]-[hw7]

--- hw_07 (BDD testing)/step_definitions/screener.steps.js
+++ hw_07 (BDD testing)/step_definitions/screener.steps.js
@@ -5,22 +5,32 @@ import PageFactory from '../page_objects/PageFactory';
 
 Then(/^the "([^"]*)" button is displayed$/, async (button) => {
   const page = await PageFactory.getPage('Screener');
-  let isButtonDisplayed = null;
-
-  switch (button) {
-    case 'Create New Screener':
-      isButtonDisplayed = await page.isCreateNewScreenButtonDisplayed();
-      expect(isButtonDisplayed).to.be.true;
-      break;
-    default:
-      throw new Error(`Option ${button} is not available`);
-  }
+  const isButtonDisplayed = await page.isButtonDisplayed(button);
+  expect(isButtonDisplayed).to.be.true;
 });
 
 Then(/^the "([^"]*)" button is clickable$/, async (button) => {
   // TODO: Implement a step that verifies that the Create New Screen button is clickable on the Screener page
+  const page = await PageFactory.getPage('Screener');
+  const isButtonClickable = await page.isButtonDisplayed(button);
+  expect(isButtonClickable).to.be.true;
 });
 
 When(/^the user clicks on the "([^"]*)" button$/, async (button) => {
   // TODO: Implement a step that clicks the button according to the given argument - "button"
+  const page = await PageFactory.getPage('Screener');
+  await page.clickOnButton(button);
+});
+
+Then(/^the "Create a New Screener" menu is displayed$/, async () => {
+  const page = await PageFactory.getPage('Screener');
+  const isCreateNewScreenMenuDisplayed = await page.isCreateNewScreenMenuDisplayed();
+  expect(isCreateNewScreenMenuDisplayed).to.be.true;
+});
+
+Then(/^the page title is "([^"]*)" on the Screener page$/, async (title) => {
+  const page = await PageFactory.getPage('Screener');
+  const expectedTitle = title;
+  const actualTitle = await page.getTitle();
+  expect(actualTitle).to.eql(expectedTitle);
 });
