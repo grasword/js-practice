7ddba8c5e903cce223bf5cf6f29cef16a80171c8
Oleg Zhmaev <grasword6@gmail.com>
2022-04-09 15:52:13 +0300

    implemented test to check site url [test]-[BDD]-[hw7]

--- hw_07 (BDD testing)/step_definitions/common.steps.js
+++ hw_07 (BDD testing)/step_definitions/common.steps.js
@@ -2,12 +2,17 @@ import { expect } from 'chai';
 import { Given, Then } from '@wdio/cucumber-framework';
 
 import PageFactory from '../page_objects/PageFactory';
+import config from '../configs/AppConfig';
 
 Given(/^the user is on the (\w+) page$/, async (pageName) => {
   const page = await PageFactory.getPage(pageName);
   await page.open();
 });
 
-Then(/^the browser URL contains "([^"]*)" on the (\w+) page$/, async (url, pageName) => {
-  // TODO: Implement a step that checks the browser url according to the given arguments - "url" and "pageName"
+Then(/^the browser URL contains "([^"]*)" on the (\w+) page$/, async (path, pageName) => {
+  // TODO: Implement a step that checks the browser url according to the given arguments - "path" and "pageName"
+  const page = await PageFactory.getPage(pageName);
+  const expectedUrl = new URL(path, config.baseUrl).href;
+  const actualUrl = await page.getUrl();
+  expect(expectedUrl).to.eql(actualUrl);
 });
